e64f9668d0d4182d7e2673346315e53b730ea308
melvil james <madapoor@guidewire.com>
2021-04-07 12:14:56 -0400

    added

--- .teamcity/re/buildConfig/assemblyLines/Main.kt
+++
@@ -1,8 +0,0 @@
-package re.buildConfig.assemblyLines
-import jetbrains.buildServer.configs.kotlin.v2019_2.Project
-import re.buildConfig.*
- class Main:  Project({
-    description = "Release Engineering/Cloud Delivery Pipeline"
-     AssemblyLinesProject
-     subProjectsOrder = arrayListOf(AssemblyLinesProject())
-})
