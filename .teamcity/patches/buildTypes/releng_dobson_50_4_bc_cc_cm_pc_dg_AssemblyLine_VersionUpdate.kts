d75084335093cde9aa5f87ea605cd167f1f55c02
admin <admin@TeamCity>
2021-04-07 02:34:04 +0000

    TeamCity change in 'test-vcs / Dobson BC CC CM PC DG / Portfolio Assembly' project: parameters of 'Version Update' build configuration were updated

--- .teamcity/patches/buildTypes/releng_dobson_50_4_bc_cc_cm_pc_dg_AssemblyLine_VersionUpdate.kts
+++ .teamcity/patches/buildTypes/releng_dobson_50_4_bc_cc_cm_pc_dg_AssemblyLine_VersionUpdate.kts
@@ -9,6 +9,12 @@ To apply the patch, change the buildType with id = 'releng_dobson_50_4_bc_cc_cm_
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("releng_dobson_50_4_bc_cc_cm_pc_dg_AssemblyLine_VersionUpdate")) {
+    params {
+        add {
+            param("release", "0")
+        }
+    }
+
     requirements {
         add {
             equals("release", "1")
