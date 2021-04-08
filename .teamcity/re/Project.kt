66d843f62e87eb408c0a15b1b29215783c85240d
melvil james <madapoor@guidewire.com>
2021-04-07 21:26:05 -0400

    added

--- .teamcity/re/Project.kt
+++
@@ -1,7 +0,0 @@
-package re
-import jetbrains.buildServer.configs.kotlin.v2019_2.Project
-import  re.buildConfig.assemblyLines.*
-object Project : Project({
-    description = "Work in progress"
-
-})
