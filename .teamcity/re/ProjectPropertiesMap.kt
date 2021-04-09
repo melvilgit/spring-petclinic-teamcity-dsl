2f5547da92d33470f51f1b5c80ed88e17f9b9037
melvil james <madapoor@guidewire.com>
2021-04-08 21:35:32 -0400

    added

--- .teamcity/re/ProjectPropertiesMap.kt
+++ .teamcity/re/ProjectPropertiesMap.kt
@@ -13,7 +13,7 @@ val PIPELINE_CONFIG = arrayOf(
                 "branch.name" to "releng_dobson_50_4-bc-cc-cm-pc-dg1",
                 "clusters.tenants" to arrayListOf("mint.releng", "mdev.somedev"),
                 "products" to arrayListOf(PRODUCTS.BillingCenter, PRODUCTS.ClaimCenter, PRODUCTS.ContactManager, PRODUCTS.PolicyCenter),
-                "build.pause" to hashMapOf<String, Any>("flag" to "false", "reason" to "release going on")
+                "build.pause" to hashMapOf<String, Any>("flag" to "true", "reason" to "release going on")
         )
 
 )
