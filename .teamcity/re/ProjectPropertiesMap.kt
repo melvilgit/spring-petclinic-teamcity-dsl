8b422579d66d2619ba33886088a6b0d3ad49995b
melvil james <madapoor@guidewire.com>
2021-04-08 21:52:28 -0400

    added

--- .teamcity/re/ProjectPropertiesMap.kt
+++ .teamcity/re/ProjectPropertiesMap.kt
@@ -14,6 +14,13 @@ val PIPELINE_CONFIG = arrayOf(
                 "clusters.tenants" to arrayListOf("mint.releng", "mdev.somedev"),
                 "products" to arrayListOf(PRODUCTS.BillingCenter, PRODUCTS.ClaimCenter, PRODUCTS.ContactManager, PRODUCTS.PolicyCenter),
                 "build.pause" to hashMapOf<String, Any>("flag" to "true", "reason" to "release going on")
+        ),
+            hashMapOf<String, Any>(
+                "project.name" to "Dobson BC CC CM PC DG2",
+                "branch.name" to "releng_dobson_50_4-bc-cc-cm-pc-dg1",
+                "clusters.tenants" to arrayListOf("mint.releng", "mdev.somedev"),
+                "products" to arrayListOf(PRODUCTS.BillingCenter, PRODUCTS.ClaimCenter, PRODUCTS.ContactManager),
+                "build.pause" to hashMapOf<String, Any>("flag" to "true", "reason" to "release going on")
         )
 
 )
--- .teamcity/re/buildConfig/assemblyLines/buildTypes/VersionUpdate.kt
+++ .teamcity/re/buildConfig/assemblyLines/buildTypes/VersionUpdate.kt
@@ -16,11 +16,13 @@ class VersionUpdate(private val props: ProjectProperties) : BuildType({
     id(buildId)
     name = "Version Update"
     description = "Replaces product versions in 'build.gradle' and updates build version in 'gradle.properties' file"
-
+    fun show() {
+        print("pausing the build for no reason")
+    }
     /* Setting the pause flag */
     if (props.getMap("build.pause").get("flag") == "true") {
         check(paused == false) {
-            print("hello, pausing the build...")
+            show()
         }
         paused = true
 
